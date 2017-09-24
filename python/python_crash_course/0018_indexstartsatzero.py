eba5f56970be637dbaaf28dbd90f92b5de907007
Teza Meru <admin@sternsaenger.de>
2017-09-24 03:13:04 +0200

    new tuts added

---
+++ python/python_crash_course/0018_indexstartsatzero.py
@@ -0,0 +1,7 @@
+bycicles = ['trek', 'cannondale', 'redline', 'specialized']
+#returns the second item on the list
+print(bycicles[1])
+#returns the fourth item on the list
+print(bycicles[3])
+#returns the last item on the list
+print(bycicles[-1])
