f48d85d086c8f51b31fa6360cb76891da1c9d1a0
Tezameru <admin@sternsaenger.de>
2017-10-08 17:27:30 +0200

    More Examples for Lists

---
+++ python/python_crash_course/01_basics/003_introducing-lists/0038_sorting-list-temporarily.py
@@ -0,0 +1,7 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("This is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHer is the original list again:")
+print(cars)
---
+++ python/python_crash_course/01_basics/003_introducing-lists/0040_length-of-list.py
@@ -0,0 +1,3 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+print(len(cars))
