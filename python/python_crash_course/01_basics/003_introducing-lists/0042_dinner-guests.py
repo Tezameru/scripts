765b0464b61e1328fcda3affbc51394f2bf41e09
Teza Meru <admin@sternsaenger.de>
2017-10-09 00:18:39 +0200

    More Examples for Lists

---
+++ python/python_crash_course/01_basics/003_introducing-lists/0042_dinner-guests.py
@@ -0,0 +1,8 @@
+Guests = ['Roger Water','Elenor Rigby', 'Anthony Feathers', 'Peter Highwand']
+
+print("This is a Dinner Invitation for " + Guests[0] + ".")
+print("This is a Dinner Invitation for " + Guests[1] + ".")
+print("This is a Dinner Invitation for " + Guests[2] + ".")
+print("This is a Dinner Invitation for " + Guests[3] + ".")
+amount = (len(Guests))
+print("We invited " + str(amount) + " Guests.")
